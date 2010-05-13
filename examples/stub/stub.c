56f62e6628117c363bdb7e38844a998a9f6fba51
brettw@chromium.org <brettw@chromium.org@d4803092-10c6-4515-1ca1-60a45aa47803>
2010-05-13 23:18:29 +0000

    Add a stub example plugin for C and C++

---
+++ examples/stub/stub.c
@@ -0,0 +1,33 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This is the simplest possible C Pepper plugin that does nothing. If you're
+// using C++, you will want to look at stub.cc which uses the more convenient
+// C++ wrappers.
+
+#include "ppapi/c/pp_module.h"
+#include "ppapi/c/ppb.h"
+#include "ppapi/c/ppp.h"
+
+PP_Module g_module_id;
+PPB_GetInterface g_get_browser_interface = NULL;
+
+PP_EXPORT int PPP_InitializeModule(PP_Module module,
+                                   PPB_GetInterface get_browser_interface) {
+  // Save the global module information for later.
+  g_module_id = module_id;
+  g_get_browser_interface = get_browser_interface;
+
+  // Returning 0 indicates success.
+  return 0;
+}
+
+PP_EXPORT void PPP_ShutdownModule() {
+}
+
+PP_EXPORT const void* PPP_GetInterface(const char interface_name) {
+  // You will normally implement a getter for at least PPP_INSTANCE_INTERFACE
+  // here.
+  return NULL;
+}
