1774d3a4f038722eacd2357c7b86b0f01d47cac3
brettw@chromium.org <brettw@chromium.org@d4803092-10c6-4515-1ca1-60a45aa47803>
2010-08-29 19:40:20 +0000

    Move file info which I forgot in the previous CL.

--- c/dev/pp_file_info_dev.h
+++ c/dev/pp_file_info_dev.h
@@ -2,31 +2,31 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef PPAPI_C_PP_FILE_INFO_H_
-#define PPAPI_C_PP_FILE_INFO_H_
+#ifndef PPAPI_C_DEV_PP_FILE_INFO_DEV_H_
+#define PPAPI_C_DEV_PP_FILE_INFO_DEV_H_
 
 #include "ppapi/c/pp_stdint.h"
 #include "ppapi/c/pp_time.h"
 
-typedef enum _pp_FileType {
+typedef enum {
   PP_FILETYPE_REGULAR,
   PP_FILETYPE_DIRECTORY,
   PP_FILETYPE_OTHER  // A catch-all for unidentified types.
-} PP_FileType;
+} PP_FileType_Dev;
 
-typedef enum _pp_FileSystemType {
+typedef enum {
   PP_FILESYSTEMTYPE_EXTERNAL,
   PP_FILESYSTEMTYPE_LOCALPERSISTENT,
   PP_FILESYSTEMTYPE_LOCALTEMPORARY
-} PP_FileSystemType;
+} PP_FileSystemType_Dev;
 
-typedef struct _pp_FileInfo {
+struct PP_FileInfo_Dev {
   int64_t size;  // Measured in bytes
-  PP_FileType type;
-  PP_FileSystemType system_type;
+  PP_FileType_Dev type;
+  PP_FileSystemType_Dev system_type;
   PP_Time creation_time;
   PP_Time last_access_time;
   PP_Time last_modified_time;
-} PP_FileInfo;
+};
 
-#endif  // PPAPI_C_PP_FILE_INFO_H_
+#endif  // PPAPI_C_DEV_PP_FILE_INFO_DEV_H_
